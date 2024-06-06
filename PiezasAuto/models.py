6c7ba50f491fef2f087fcf1a1560d35c5bf94939
Emiliano Murua <emilianomuruag97@gmail.com>
2024-06-06 15:41:47 -0300

    Prueba piezas

---
+++ PiezasAuto/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+# Create your models here.
+
+class PiezaSoldadura(models.Model):
+
+    Code=models.CharField(max_length=40)
+    Client=models.CharField(max_length=40)
+    Cadencia=models.CharField(max_length=40)
+    NumDoc=models.CharField(max_length=40) 
+    Minuteria=models.CharField(max_length=40)
+
+
+class PiezaPrensa(models.Model):
+
+    Code=models.CharField(max_length=40)
+    Client=models.CharField(max_length=40)
+    Cadencia=models.CharField(max_length=40)
+    NumDoc=models.CharField(max_length=40) 
+
+
+class DeclararPieza(models.Model):
+
+    Code=models.CharField(max_length=40)
+    Client=models.CharField(max_length=40)
+    Cadencia=models.CharField(max_length=40)
+    NumDoc=models.CharField(max_length=40) 
+    Cantidad=models.CharField(max_length=40) 
+    Horas=models.CharField(max_length=40) 
